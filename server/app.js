6e6c1464a6e5dc7d6b62b198ff690275b04b624e
= <=>
2020-01-03 17:53:49 +0100

    maj routes REST

--- server/app.js
+++ server/app.js
@@ -30,42 +30,27 @@ app.post('/code', async (req, res) => {
     responseType: 'json'
   })
 
-  var info = result.data
-  var product = info.product
+  var item = result.data.product
 
-  var namebis = product.generic_name
-  var name = product.product_name
-  var nova = product.nova_group
-  var nutrition = product.nutrition_grade_fr
-  var palme = product.ingredients_from_palm_oil_n
-  var palmebis = product.ingredients_that_may_be_from_palm_oil_n
-  var picture = product.image_url
-  var additives = product.additives_n
+  // console.log(item.code)
 
-  console.log('name : ' + name)
-  console.log('namebis : ' + namebis)
-  console.log('nova : ' + nova)
-  console.log('nutrition : ' + nutrition)
-  console.log('palme : ' + palme)
-  console.log('palmebis : ' + palmebis)
-  console.log('picture : ' + picture)
-  console.log('additives : ' + additives)
-
-  // two possible fields for name
-  if (name === null || name === '') {
-    name = namebis
+  var element = {
+    code: item.code,
+    product_name: item.product_name,
+    origins: item.origins.split(','),
+    manufacturing_places: item.manufacturing_places.split(','),
+    ingredients_text: item.ingredients_text.split(','),
+    additives_n: item.additives_n,
+    additives_tags: item.additives_tags,
+    ingredients_from_palm_oil_n: item.ingredients_from_palm_oil_n,
+    nutriscore_score: item.nutriscore_score,
+    nutriscore_grade: item.nutriscore_grade,
+    nova_group: item.nova_group,
+    image_url: item.image_url,
+    energy_100g: item.energy_100g
   }
 
-  var fields = [
-    { name: name,
-      nova: nova,
-      nutrition: nutrition,
-      palme: palme,
-      palmebis: palmebis,
-      picture: picture,
-      additives: additives }
-  ]
-  res.send(fields)
+  res.send(element)
 })
 
 app.post('/search', async (req, res) => {
@@ -85,34 +70,21 @@ app.post('/search', async (req, res) => {
     .on('end', () => {
       for (var i = 0; i < results.length; i++) {
         var item = results[i]
-        const code = item['code']
-        const product_name = item['product_name']
-        const origins = item['origins'].split(',')
-        const manufacturing_places = item['manufacturing_places'].split(',')
-        const ingredients_text = item['ingredients_text'].split(', ')
-        const additives_n = item['additives_n']
-        const additives_tags = item['additives_tags'].split(',')
-        const ingredients_from_palm_oil_n = item['ingredients_from_palm_oil_n']
-        const nutriscore_score = item['nutriscore_score']
-        const nutriscore_grade = item['nutriscore_grade']
-        const nova_group = item['nova_group']
-        const image_url = item['image_url']
-        const energy_100g = item['energy_100g'][0]
 
         list.push({
-          code: code,
-          product_name: product_name,
-          origins: origins,
-          manufacturing_places: manufacturing_places,
-          ingredients_text: ingredients_text,
-          additives_n: additives_n,
-          additives_tags: additives_tags,
-          ingredients_from_palm_oil_n: ingredients_from_palm_oil_n,
-          nutriscore_score: nutriscore_score,
-          nutriscore_grade: nutriscore_grade,
-          nova_group: nova_group,
-          image_url: image_url,
-          energy_100g: energy_100g
+          code: item.code,
+          product_name: item.product_name,
+          origins: item.origins.split(','),
+          manufacturing_places: item.manufacturing_places.split(','),
+          ingredients_text: item.ingredients_text.split(','),
+          additives_n: item.additives_n,
+          additives_tags: item.additives_tags,
+          ingredients_from_palm_oil_n: item.ingredients_from_palm_oil_n,
+          nutriscore_score: item.nutriscore_score,
+          nutriscore_grade: item.nutriscore_grade,
+          nova_group: item.nova_group,
+          image_url: item.image_url,
+          energy_100g: item.energy_100g
         })
       }
 
